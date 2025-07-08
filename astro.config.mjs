67c3a6a95e0ad64414f9146caab777762b0fbf0e
za-ibrahim <ibrahim@zeptoapps.com>
2025-07-08 19:11:56 +0600

    updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  experimental: {
+    fonts: [{
+      provider: fontProviders.google(),
+      name: "Lexend",
+      cssVariable: "--font-roboto"
+    }]
+  },
   vite: {
     plugins: [tailwindcss()]
   }
